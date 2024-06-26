f9ab8ce9fe6e20f82674ea9d6f037613b246292e
C4lumny <ospinonathan@gmail.com>
2024-06-26 15:58:15 -0500

    feat(migrations): Migrations done, these arent tested yet

---
+++ database/migrations/2024_06_26_191908_create_card_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('card', function (Blueprint $table) {
+            $table->id();
+            $table->string('number')->unique();
+            $table->string('purpose');
+            $table->boolean('income');
+            $table->boolean('expenditure');
+            $table->double('amount');
+            $table->foreignId('card_type_id')->constrained()->onDelete('cascade');
+            $table->foreignId('supplier_id')->constrained()->onDelete('cascade');
+            $table->foreignId('bank_id')->constrained()->onDelete('cascade');
+            $table->foreignId('wallet_id')->constrained()->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('card');
+    }
+};
